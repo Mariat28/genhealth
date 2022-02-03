5fd6d34905acbb2e63aac6d077dbd7be544a052f
Mariat28 <mariatndagire28@gmail.com>
2022-02-03 18:07:34 +0300

    login page responsiveness

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -31,7 +31,7 @@ function LoginPage() {
   return(
     <div className='h-screen  flex justify-center items-center bg-blue-50'>
         
-      <div className=' lg:h-3/4 h-1/2 2xl:h-1/2   shadow-2xl rounded-[30px] overflow-hidden 2xl:w-1/2 w-3/4 grid md:grid-cols-5 '>
+      <div className=' lg:h-3/4 h-1/2    shadow-2xl rounded-[30px] overflow-hidden 2xl:w-1/2 w-3/4 grid md:grid-cols-5 '>
         <div className='col-span-2 bg-white h-full p-8 flex flex-col justify-center'>
           <div className=' mt-0 ml-4'>
             {/* <img src={Logo} alt="app-logo" className='h-10 w-10 hidden'/> */}
@@ -45,7 +45,7 @@ function LoginPage() {
             </div>
             <div className='ml-0 mt-2 text-sm flex flex-col p-4'>
               <label className='text-sm'>Enter your password</label>
-              <input type='password' placeholder='atleast 8 characters' name=' password' className='rounded-sm border  h-10 mt-2 p-1' value={password || ''} required   onChange={handlePasswordChange}/>
+              <input type='password' placeholder='atleast 8 characters' name=' password' className='rounded-sm border  h-10 mt-2 p-1 form-control' value={password || ''} required   onChange={handlePasswordChange}/>
             </div>
             <div className='p-4 flex flex-col items-start'>
               <button type='submit' className='bg-blue-800 p-2 rounded-md w-full text-white text-center block hover:bg-blue-700'>Log in</button>
@@ -60,8 +60,8 @@ function LoginPage() {
           </form>
         </div>
         <div className='col-span-3 bg-blue-100  h-full flex items-center flex-col justify-center '>
-          <p className='txet-slate-700 text-lg mb-4'>Nice to see you again </p>
-          <p className='text-3xl text-blue-800 mb-10 font-semibold'>Welcome Back!</p>
+          {/* <p className='txet-slate-700 text-lg mb-4'>Nice to see you again </p> */}
+          <p className='text-3xl text-blue-800 mb-10 font-semibold'>Welcome back!</p>
           <img src={heroImg} alt="app-logo" className='h-80 w-100'/>
         </div>
       </div>
