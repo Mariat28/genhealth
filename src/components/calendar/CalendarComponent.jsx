fea26bc22823b23172240fdf56cbb4045813305d
Mariat Ndagire <mariatndagire28@gmail.com>
2022-03-26 10:27:42 +0300

    calendar view logic

--- src/components/calendar/CalendarComponent.jsx
+++ src/components/calendar/CalendarComponent.jsx
@@ -4,31 +4,38 @@ import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
 
 function CalendarView(props) {
+  const week={
+    hourStart: 6,
+    hourEnd: 19
+
+  };
+  const schedules =[
+    {
+      id: '1',
+      calendarId: '0',
+      title : 'Study for test',
+      start : '2022-03-26T09:00:00.000Z',
+      end : '2022-03-26T09:30:00.000Z',
+      categoty: 'time',
+    }
+  ]
   return(
     <Calendar
-      height="100%"
+      height="1000px"
+      scheduleView
       view = {props.selectedView}
       taskView={false}
-      calendars={[
-        {
-          id: '0',
-          name: 'Private',
-          bgColor: '#9e5fff',
-          borderColor: '#9e5fff'
-        },
-        {
-          id: '1',
-          name: 'Company',
-          bgColor: '#00a9ff',
-          borderColor: '#00a9ff'
-        }
-      ]}
+      week={week}
+      useCreationPopup = {true}
+      useDetailPopup = {true}
       disableDblClick={true}
       disableClick={false}
       isReadOnly={false}
       month={{
         startDayOfWeek: 0
       }}
+      schedules={schedules}
+      
     />
   )
 }
